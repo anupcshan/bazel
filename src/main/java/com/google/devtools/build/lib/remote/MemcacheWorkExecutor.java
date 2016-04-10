b8db7c95de25a826232bbace71cde860525eb1e6
Anup Chenthamarakshan <anupc@dropbox.com>
2016-04-10 11:22:44 -0700

    Remote work execution - Completely stolen from https://bazel-review.googlesource.com/#/c/2830/3

---
+++ src/main/java/com/google/devtools/build/lib/remote/MemcacheWorkExecutor.java
@@ -0,0 +1,165 @@
+// Copyright 2016 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.devtools.build.lib.remote;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.devtools.build.lib.actions.ActionInput;
+import com.google.devtools.build.lib.actions.ActionInputFileCache;
+import com.google.devtools.build.lib.concurrent.ThreadSafety.ThreadSafe;
+import com.google.devtools.build.lib.remote.RemoteProtocol.FileEntry;
+import com.google.devtools.build.lib.remote.RemoteProtocol.RemoteWorkRequest;
+import com.google.devtools.build.lib.shell.Command;
+import com.google.devtools.build.lib.shell.CommandException;
+import com.google.devtools.build.lib.shell.CommandResult;
+import com.google.devtools.build.lib.shell.TerminationStatus;
+import com.google.devtools.build.lib.vfs.FileSystemUtils;
+import com.google.devtools.build.lib.vfs.Path;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+/**
+ * Implementation of {@link RemoteWorkExecutor} that uses MemcacheActionCache and protobufs for
+ * communicating the work, inputs and outputs.
+ */
+@ThreadSafe
+public class MemcacheWorkExecutor implements RemoteWorkExecutor {
+  protected final ListeningExecutorService executorService;
+
+  /**
+   * A cache used to store the input and output files as well as the build status
+   * of the remote work.
+   */
+  protected final MemcacheActionCache cache;
+
+  /**
+   * Execution root for running this work locally.
+   */
+  private final Path execRoot;
+
+  private static final int MAX_WORK_SIZE_BYTES = 1024 * 1024 * 512;
+
+  public MemcacheWorkExecutor(MemcacheActionCache cache, ListeningExecutorService executorService,
+                              Path execRoot) {
+    this.cache = cache;
+    this.executorService = executorService;
+    this.execRoot = execRoot;
+  }
+
+  @Override
+  public ListenableFuture<Response> submit(
+      Path execRoot,
+      ActionInputFileCache actionCache,
+      String actionOutputKey,
+      Collection<String> arguments,
+      Collection<ActionInput> inputs,
+      ImmutableMap<String, String> environment,
+      Collection<? extends ActionInput> outputs,
+      int timeout) throws IOException {
+    RemoteWorkRequest.Builder work = RemoteWorkRequest.newBuilder();
+    work.setOutputKey(actionOutputKey);
+
+    long workSize = 0;
+    for (ActionInput input : inputs) {
+      Path file = execRoot.getRelative(input.getExecPathString());
+      if (file.isDirectory()) {
+        continue;
+      }
+      workSize += file.getFileSize();
+    }
+
+    if (workSize > MAX_WORK_SIZE_BYTES) {
+      throw new WorkTooLargeException("Work is too large: " + workSize + " bytes.");
+    }
+
+    // Save all input files to cache.
+    for (ActionInput input : inputs) {
+      Path file = execRoot.getRelative(input.getExecPathString());
+           
+      if (file.isDirectory()) {
+        // TODO(alpha): Need to handle these cases. 
+        continue;
+      }
+ 
+      String contentKey = cache.putFileIfNotExist(actionCache, input);
+      work.addInputFilesBuilder()
+          .setPath(input.getExecPathString())
+          .setContentKey(contentKey)
+          .setExecutable(file.isExecutable());
+    }
+
+    work.addAllArguments(arguments);
+    work.getMutableEnvironment().putAll(environment);
+    for (ActionInput output : outputs) {
+      work.addOutputFilesBuilder()
+          .setPath(output.getExecPathString());
+    }
+    work.setTimeout(timeout);
+    return submit(work.build());
+  }
+
+  /**
+   * Submit a work in the form of protobuf. This method executes the work locally.
+   */
+  public ListenableFuture<Response> submit(RemoteWorkRequest work) throws IOException {
+    return executorService.submit(new Callable<Response>(){
+        public Response call() throws IOException {
+          ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+          ByteArrayOutputStream stderr = new ByteArrayOutputStream();
+          try {
+            // Prepare directories and input files.
+            for (FileEntry input : work.getInputFilesList()) {
+              Path file = execRoot.getRelative(input.getPath());
+              FileSystemUtils.createDirectoryAndParents(file.getParentDirectory());
+              cache.writeFile(input.getContentKey(), file, input.getExecutable());
+            }
+
+            // Prepare directories for output files.
+            List<Path> outputs = new ArrayList<>();
+            for (FileEntry output : work.getOutputFilesList()) {
+              Path file = execRoot.getRelative(output.getPath());
+              outputs.add(file);
+              FileSystemUtils.createDirectoryAndParents(file.getParentDirectory());
+            }
+              
+            Command cmd = new Command(work.getArgumentsList().toArray(new String[]{}),
+                                      work.getEnvironment(),
+                                      new File(execRoot.getPathString()));
+            CommandResult result = cmd.execute(Command.NO_INPUT,
+                                               Command.NO_OBSERVER,
+                                               stdout,
+                                               stderr,
+                                               true);
+            cache.putActionOutput(work.getOutputKey(), execRoot, outputs);
+            return new Response(result.getTerminationStatus().success(),
+                                stdout.toString(),
+                                stderr.toString(),
+                                "");
+          } catch (CommandException e) {
+            return new Response(false, stdout.toString(), stderr.toString(), e.toString());
+          }
+        }
+      });
+  }
+}
+
---
+++ src/main/java/com/google/devtools/build/lib/remote/RestWorkExecutor.java
@@ -0,0 +1,83 @@
+// Copyright 2015 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.devtools.build.lib.remote;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.devtools.build.lib.concurrent.ThreadSafety.ThreadSafe;
+import com.google.devtools.build.lib.remote.RemoteProtocol.RemoteWorkRequest;
+import com.google.devtools.build.lib.remote.RemoteProtocol.RemoteWorkResponse;
+import com.google.devtools.build.lib.vfs.Path;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+/**
+ * A remote work executor that is based on REST. A REST server that is compatible
+ * with this class is implemented in src/tools/build_worker.
+ */
+@ThreadSafe
+class RestWorkExecutor extends MemcacheWorkExecutor {
+  /**
+   * This is a thread safe http client although performance might not be ideal.
+   * TODO(alpha): Consider having a dedicated thread for this http communication.
+   */
+  private final URL workerUrl;
+
+  public RestWorkExecutor(MemcacheActionCache cache,
+                          URL restWorkerUrl) {
+    // |execRoot| is only used for running the work locally which will not happen for class.
+    super(cache, MoreExecutors.newDirectExecutorService(), null);
+    this.workerUrl = restWorkerUrl;
+  }
+
+  @Override
+  public ListenableFuture<Response> submit(RemoteWorkRequest work) throws IOException {
+    return executorService.submit(new Callable<Response>(){
+      public Response call() throws URISyntaxException,
+          InvalidProtocolBufferException, IOException {
+        // It is probably not ideal having one http client per Callable.
+        // TODO(alpha): Should have a connection pool to get better performance.
+        final HttpClient httpClient = new DefaultHttpClient();
+        HttpPost post = new HttpPost(workerUrl.toURI());
+        StringEntity workJson = new StringEntity(JsonFormat.printer().print(work), "UTF-8");
+        post.addHeader("content-type", "application/json");
+        post.setEntity(workJson);
+        HttpResponse response = httpClient.execute(post);
+
+        // The REST call is blocking and returns when the work is done.
+        // TODO(alpha): Change this to an async call by listening to the completion event.
+        String responseJson = new BasicResponseHandler().handleResponse(response);
+        RemoteWorkResponse.Builder workResponse = RemoteWorkResponse.newBuilder();
+        JsonFormat.parser().merge(responseJson, workResponse);
+        return new Response(workResponse.getSuccess(), workResponse.getOut(),
+                            workResponse.getErr(), workResponse.getException());
+      }
+    });
+  }
+}
